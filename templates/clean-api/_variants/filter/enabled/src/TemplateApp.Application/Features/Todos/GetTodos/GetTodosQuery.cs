90a2bba3ceb5d80d290d157ea82acf182a316799
John Vo <131334376+johnvo402@users.noreply.github.com>
2026-09-16 03:45:52 +0700

    fix: remove legacy todo filter query

--- templates/clean-api/_variants/filter/enabled/src/TemplateApp.Application/Features/Todos/GetTodos/GetTodosQuery.cs
+++
@@ -1,8 +0,0 @@
-using Mediator;
-using TemplateApp.Application.Common.Querying;
-using TemplateApp.Application.Common.Results;
-
-namespace TemplateApp.Application.Features.Todos.GetTodos;
-
-public sealed record GetTodosQuery(QueryParameters Query)
-    : IQuery<Result<PaginationResponse<GetTodosResponse>>>;
