173180891e585a453b60f1ddf067fc321df3a3f0
John Vo <131334376+johnvo402@users.noreply.github.com>
2026-09-16 03:15:10 +0700

    refactor: expose business authorization policies

--- templates/clean-api/src/TemplateApp.Api/Authentication/AppPolicies.cs
+++ templates/clean-api/src/TemplateApp.Api/Authentication/AppPolicies.cs
@@ -4,9 +4,25 @@ namespace TemplateApp.Api.Authentication;
 
 public static class AppPolicies
 {
-    public const string TodosRead = AppPermissions.TodosRead;
-    public const string TodosWrite = AppPermissions.TodosWrite;
-    public const string UsersRead = AppPermissions.UsersRead;
-    public const string UsersManage = AppPermissions.UsersManage;
+    public const string DashboardView = AppPermissions.DashboardView;
+
+    public const string ProductsView = AppPermissions.ProductsView;
+    public const string ProductsCreate = AppPermissions.ProductsCreate;
+    public const string ProductsUpdate = AppPermissions.ProductsUpdate;
+    public const string ProductsDelete = AppPermissions.ProductsDelete;
+
+    public const string OrdersView = AppPermissions.OrdersView;
+    public const string OrdersCreate = AppPermissions.OrdersCreate;
+    public const string OrdersUpdateStatus = AppPermissions.OrdersUpdateStatus;
+    public const string OrdersCancel = AppPermissions.OrdersCancel;
+
+    public const string EmployeesView = AppPermissions.EmployeesView;
+    public const string EmployeesCreate = AppPermissions.EmployeesCreate;
+    public const string EmployeesUpdate = AppPermissions.EmployeesUpdate;
+    public const string EmployeesChangeRole = AppPermissions.EmployeesChangeRole;
+
+    public const string ReportsView = AppPermissions.ReportsView;
+    public const string SettingsView = AppPermissions.SettingsView;
+    public const string SettingsUpdate = AppPermissions.SettingsUpdate;
     public const string AiGenerate = AppPermissions.AiGenerate;
 }
