d9faedac7817da574f179823ff071b33f8509692
John Vo <131334376+johnvo402@users.noreply.github.com>
2026-09-17 02:24:52 +0700

    refactor: remove remaining Todo domain artifacts

--- templates/clean-api/src/TemplateApp.Domain/Todos/Events/TodoCreatedDomainEvent.cs
+++
@@ -1,8 +0,0 @@
-using TemplateApp.Domain.Common;
-
-namespace TemplateApp.Domain.Todos.Events;
-
-public sealed record TodoCreatedDomainEvent(TodoId TodoId) : IDomainEvent
-{
-    public DateTimeOffset OccurredOnUtc { get; } = DateTimeOffset.UtcNow;
-}
